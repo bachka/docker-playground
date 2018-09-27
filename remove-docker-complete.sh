40bb9b2e7538e8946d620fbfee522ecadb87c134
bachka <ichinnorovb@gmail.com>
2018-09-27 10:47:00 +0200

    adding script to clean up system from docker

---
+++ remove-docker-complete.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+dpkg -l | grep -i docker
+# sudo apt-get purge <package>
+
+sudo apt-get purge -y docker-engine docker docker.io docker-ce
+sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce
+
+sudo rm -rf /var/lib/docker
+sudo rm /etc/apparmor.d/docker
+sudo groupdel docker
+sudo rm -rf /var/run/docker.sock
+
+
